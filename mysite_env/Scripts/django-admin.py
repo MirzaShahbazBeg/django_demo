751faa61fbb7a6c151cfc6d4db31db170237ac94
Mirza Shahbaz Beg <begmirzashahbaz@gmail.com>
2020-07-06 22:21:43 +0530

    initial commit

---
+++ mysite_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mirza shahbaz beg\documents\django_directory\mysite_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
